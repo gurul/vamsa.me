98e193976a744fc117461be5d6d7c4a07d0f757e
Agastya Mongia <agastyam@outlook.com>
2024-07-24 16:26:11 -0700

    Added timebox page

--- vamsa-expo/app/(tabs)/_layout.tsx
+++ vamsa-expo/app/(tabs)/_layout.tsx
@@ -4,13 +4,13 @@ import { Tabs } from 'expo-router';
 export default function TabLayout() {
   return (
     <Tabs screenOptions={{ tabBarActiveTintColor: 'blue' }}>
-    <Tabs.Screen 
-      name="profile"
-      options={{
-        title: 'Profile',
-        tabBarIcon: ({ color }) => <FontAwesome size={28} name="user" color={color} />,
-      }}
-    />
+      <Tabs.Screen
+        name="timebox"
+        options={{
+          title: 'Memories',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="camera" color={color} />,
+        }}
+      />
       <Tabs.Screen
         name="index"
         options={{
@@ -18,13 +18,14 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
         }}
       />
-      <Tabs.Screen
-        name="about"
-        options={{
-          title: 'About',
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="cog" color={color} />,
-        }}
-      />
+      
+      <Tabs.Screen 
+      name="profile"
+      options={{
+        title: 'Profile',
+        tabBarIcon: ({ color }) => <FontAwesome size={28} name="user" color={color} />,
+      }}
+    />
     </Tabs>
   );
 }
